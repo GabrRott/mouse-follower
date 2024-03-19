db71c8b88a75f1ae700f31dd8f02a5816291d299
GabrRott <gabrielroatta@gmail.com>
2024-03-19 12:51:18 -0300

    First Steps

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+
+
+function App() {
+  const [enabled, setEnabled]= useState(false)
+
+  useEffect(()=>{
+    console.log('effect', {enabled})
+
+    const handleMove = (event) =>{
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY})
+    }
+
+
+  }, [enabled])
+  return (
+    <main>
+      <div style={{
+        position:'absolute',
+        backgroundColor:'#09f',
+        boderRaius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}/>
+    <button onClick={() =>setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+    
+    </main>
+  )
+  }  
+
+export default App
